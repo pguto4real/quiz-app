8d9dac1a78ea6924959cc7406d8563a88e1d22e7
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-29 20:56:47 -0600

    dynamic

--- src/Components/QuizFinished.jsx
+++ src/Components/QuizFinished.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const QuizFinished = () => {
   return (
-    <div>QuizFinished</div>
+    <div className="card-body  items-center">
+                        <h2 className="card-title">Quiz Finished</h2>
+                        <p>You Scored : {score} Out of {question_length} Questions</p>
+                        <button className="btn btn__quiz btn-active" onClick={handleRestart}>Restart Quiz</button>
+                    </div>
   )
 }
